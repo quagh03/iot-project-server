d8db76d48dd69a01222ddcd20ea9ad3c21d163f3
Huy Le Quang <91176131+quagh03@users.noreply.github.com>
2026-06-25 21:19:33 +0700

    initial commit

---
+++ src/main/java/com/huylq/iotprojectserver/IotProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.huylq.iotprojectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotProjectServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IotProjectServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/huylq/iotprojectserver/IotProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huylq.iotprojectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotProjectServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
